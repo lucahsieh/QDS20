994cb1d20866e2a84b362d8be33e3ef495f5734f
Jeffry Lam Tang <jeffrylam_96@hotmail.ca>
2020-01-11 17:48:32 -0800

    added folder for javascript, suggestedprice js file, suggestedprice function to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+// declare var hello: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qds20';
+  // result = hello();
+  constructor(){
+    // this.result;
+  }
 }
